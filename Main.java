5cb4c8a2d03f610486d3dc49b1da96f0aa058a80
kishan patel <kishan.patel@mail.mcgill.ca>
2012-11-06 15:38:16 -0500

    Performs US localization.

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
 import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
@@ -9,40 +11,27 @@ import lejos.util.TimerListener;
 public class Main {
 
 	public static void main(String[] args){
-		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
-		LightSensor ls = new LightSensor(SensorPort.S2);
-		TwoWheeledRobot patBot = new TwoWheeledRobot(Motor.A, Motor.B);
-		Odometer odo = new Odometer(patBot, true);
-		LCDInfo lcd = new LCDInfo(odo);
-		USLocalizer usl = new USLocalizer(odo, us,
-				USLocalizer.LocalizationType.FALLING_EDGE);
-		LightFinder lf = new LightFinder(odo, us, ls);
-		Navigation nav = new Navigation(odo);
-		OdometryCorrector odometryCorrection = new OdometryCorrector(odo);
-		BrickCommunicator brickCommunicator = new BrickCommunicator();
-		BeaconLifter beaconLifter = new BeaconLifter();
-		SearchAlgo sAlgo = new SearchAlgo();
-		boolean beaconFound = false;
-		TimerListener hcl = new HeadingCorrector();
-		TimerListener ocl = new OdometryCorrector(odo);
-		TimerListener oal = new ObstacleAvoider(us,odo,nav);
-		Timer hc;
-		Timer oc;
-		Timer oa;
+		int buttonChoice;
+		LCD.clear();
+		do{
+			LCD.drawString("Left - Do Us. Loca." , 0, 0);
+			buttonChoice = Button.waitForAnyPress();
+		}while(buttonChoice!=Button.ID_RIGHT&&buttonChoice!=Button.ID_LEFT);
+
 		
-		while(!beaconFound){
-			lf.findLightSourceHeading();
-			if(lf.LightSourceFound()){
-				lf.turnToLightSourceHeading();
-				hc = new Timer(10,hcl);
-				oc = new Timer(10,ocl);
-				oa = new Timer(10,oal);
-				hc.start();
-				oc.start();
-				oa.start();
-			}else{
-				sAlgo.navigateToNextCoordinate();
-			}
+		if(buttonChoice == Button.ID_LEFT){
+			UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+			LightSensor ls = new LightSensor(SensorPort.S3);
+			TwoWheeledRobot patBot = new TwoWheeledRobot(Motor.A, Motor.B);
+			Odometer odo = new Odometer(patBot, true);
+			LCDInfo lcd = new LCDInfo(odo);
+			USLocalizer usl = new USLocalizer(odo, us,
+					USLocalizer.LocalizationType.FALLING_EDGE);
+			//LightFinder lf = new LightFinder(odo, us, ls);
+			usl.doLocalization();
 		}
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		System.exit(0);
+		
 	}
 }
